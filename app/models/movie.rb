56fa02d3eb174b4cde262bffb5e9d8d174cf2d50
Guilherme Rocha <jguilherme.1997@gmail.com>
2024-10-22 20:44:19 -0300

    Adição de regra para que a coluna Type não fosse reconhecida como fins de herança, problema ocorrendo ao usar o GET na API

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
     validates :show_id, :title, :release_year, :country, :listed_in, :description, presence: true
     validates :show_id, uniqueness: true
+
+    self.inheritance_column = :_type_disabled
 end
